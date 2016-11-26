77d12b692a7e4a7f787fefd211ffd2c56e46d8bb
Samuel Parra <samuelparra1@gmail.com>
2016-11-26 12:38:16 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var scanf = require("scanf");
 var mongojs = require("mongojs");
 var myParser = require ("body-parser");
 var answer;
-var tq1=false,tq2=false,verify,finded=false;
 
 
 
@@ -36,60 +35,12 @@ app.get("/get-characters", (req, res) => {
     
 });
 
-
-
  app.post("/ans", function(req, res) {
       answer = req.body.answer;
       console.log(answer); 
      res.send({});
-  if(finded!=true){
-    if((answer === "yes" && verify!=false) || (answer === "no" && verify==true))  {
-        verify=true;
-        if(tq1==false)
-        sendQuestion("Is your character a human?");
-        if(answer === "yes" && tq1==true ){
-            //codigo que crea la copia de humano real
-            //question_maker(question_human_real, dbcopy)
-            console.log("Your group is human real");
-            finded=true;
-        }else if (answer === "no" && tq1==true ) {
-            //codigo que crea la copia de animal real
-            //question_maker(question_animal_real, dbcopy)
-            console.log("Your group is animal real");
-             finded=true;
-        }
-        tq1=true;
-    }else{
-    verify=false;
-      if(tq1==false)
-        sendQuestion("Is your character a human? 2");
-        if(answer === "yes" && tq1==true && tq2!=true){
-            //codigo que crea copia de humano animado
-            //question_maker(question_human_animated, dbcopy)
-            console.log("Your group is human animated");
-            finded=true;
-        }else if( (answer === "no" && tq1==true) ||(answer === "yes" && tq1==true) ){
-            if(tq2==false)
-            sendQuestion("Is your character an Animal?");
-            if(answer === "yes" && tq2==true){
-                //codigo que crea la copia aniaml animado
-                //question_maker(question_animal_animated, dbcopy)
-                console.log("Your group is Animal animated");
-                finded=true;
-            }else if(answer === "no" && tq2==true){
-                //codigo que crea la copia otros
-                //question_maker(question_other_animated, dbcopy)
-                console.log("Your group is other animated");
-                finded=true;
-            }
-            tq2=true;
-        }
-        tq1=true;
-    }
-}else{
-    console.log("Chamo ya lo encontré")
-}
-
+    
+ //sendQuestion(questions_Human_real[randNumber()].question);
 });
 
 
@@ -98,28 +49,82 @@ console.log(answer);
 console.log("1337 is the port!");
 
 // Questions
-var questions_Human_real = [
-	{"question":"Is your character a ", "atribute":"age" , "value":"tba" , "efective":0, "isBool":false,"quantity":0},
-	{"question":"Does your character have a missing family member?" ,"atribute":"missing_family_member","value":"yes","efective":0,"isBool":true,"quantity":0},
-	{"question":"Is your character in love?" , "atribute":"in_love" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Does your character knows how to" , "atribute":"talent" , "value":"tba" , "efective":0,"isBool":false,"quantity":0},
-	{"question":"Does your character knows how to" , "atribute":"talent" , "value":"tba" , "efective":0,"isBool":false,"quantity":0},
-	{"question":"Is your character a " , "atribute":"occupation" , "value":"tba" , "efective":0,"isBool":false,"quantity":0},
-	{"question":"Does your character a villain?" , "atribute":"villain" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Is your character dumb?" , "atribute":"dumb" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Is your character mean?" , "atribute":"mean" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Is your character funny?" , "atribute":"funny" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Does your character have hair color " , "atribute":"hair_color" , "value":"tba" , "efective":0, "isBool":false,"quantity":0},
-	{"question":"Is your character a " , "atribute":"gender" , "value":"tba" , "efective":0, "isBool":false,"quantity":0},
-	{"question":"Does your character have skin color " , "atribute":"skin_color" , "value":"tba" , "efective":0, "isBool":false,"quantity":0},
-	{"question":"Is your character " , "atribute":"weight" , "value":"tba" , "efective":0, "isBool":false,"quantity":0},
-	{"question":"Does your character have a catch frase?" , "atribute":"catch_phrase" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Does your character have his/her name in the show title?" , "atribute":"name_in_show_title" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Is your character from an educational show?" , "atribute":"educational" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Does your character wears glasses?" , "atribute":"glasses" , "value":"yes" , "efective":0,"isBool":true,"quantity":0},
-	{"question":"Is your character from " , "atribute":"channel" , "value":"tba" , "efective":0, "isBool":false,"quantity":0} ];
+var questions_human_real = [
+	{"question":"Is your character a ", "atribute":"age" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Does your character have a missing family member?" ,"atribute":"missing_family_member","value":"yes","efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character in love?" , "atribute":"in_love" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character knows how to" , "atribute":"talent" , "value":"tba" , "efective":0,"isBool":false,"percentage":0},
+	{"question":"Does your character knows how to" , "atribute":"talent" , "value":"tba" , "efective":0,"isBool":false,"percentage":0},
+	{"question":"Is your character a " , "atribute":"occupation" , "value":"tba" , "efective":0,"isBool":false,"percentage":0},
+	{"question":"Does your character a villain?" , "atribute":"villain" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character dumb?" , "atribute":"dumb" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character mean?" , "atribute":"mean" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character funny?" , "atribute":"funny" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character have hair color " , "atribute":"hair_color" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Is your character a " , "atribute":"gender" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Does your character have skin color " , "atribute":"skin_color" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Is your character " , "atribute":"weight" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Does your character have a catch frase?" , "atribute":"catch_phrase" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character have his/her name in the show title?" , "atribute":"name_in_show_title" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character from an educational show?" , "atribute":"educational" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character wears glasses?" , "atribute":"glasses" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character from " , "atribute":"channel" , "value":"tba" , "efective":0, "isBool":false,"percentage":0} ];
 
+var questions_human_animated = [
+    {"question":"Is your character a ", "atribute":"age" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Does your character have a missing family member?" ,"atribute":"missing_family_member","value":"yes","efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character in love?" , "atribute":"in_love" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character knows how to" , "atribute":"talent" , "value":"tba" , "efective":0,"isBool":false,"percentage":0},
+	{"question":"Does your character knows how to" , "atribute":"talent" , "value":"tba" , "efective":0,"isBool":false,"percentage":0},
+	{"question":"Is your character a " , "atribute":"occupation" , "value":"tba" , "efective":0,"isBool":false,"percentage":0},
+	{"question":"Does your character a villain?" , "atribute":"villain" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character dumb?" , "atribute":"dumb" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character mean?" , "atribute":"mean" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character funny?" , "atribute":"funny" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character have hair color " , "atribute":"hair_color" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Is your character a " , "atribute":"gender" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Does your character have skin color " , "atribute":"skin_color" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Is your character " , "atribute":"weight" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+	{"question":"Does your character have a catch frase?" , "atribute":"catch_phrase" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character have his/her name in the show title?" , "atribute":"name_in_show_title" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character from an educational show?" , "atribute":"educational" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Does your character wears glasses?" , "atribute":"glasses" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+	{"question":"Is your character from " , "atribute":"channel" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+    {"question":"Does your character have superpower?" , "atribute":"superpower" , "value":"yes" , "efective":0, "isBool":true,"percentage":0},
+    {"question":"Does your character have one set of clothes? " , "atribute":"one_set_clothes" , "value":"yes" , "efective":0, "isBool":true,"percentage":0}];
 
+var question_animal_real = [
+    {"question":"Is your character a " , "atribute":"gender" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+    {"question":"Does your character have hair color " , "atribute":"hair_color" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+    {"question":"Is your character " , "atribute":"type" , "value":"tba" , "efective":0, "isBool":false, "percentage":0},
+    {"question":"Does your character talks?" , "atribute":"talks" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Is your character " , "atribute":"personality" , "value":"tba" , "efective":0, "isBool":false, "percentage":0},
+    {"question":"Does your character have a catch phrase?" , "atribute":"catch_phrase" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Does your character have his/her name in the show title?" , "atribute":"name_in_show_title" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Is your character from an educational show?" , "atribute":"educational" , "value":"yes" , "efective":0,"isBool":true,"percentage":0}
+]
+
+var question_animal_animated = [
+    {"question":"Is your character a " , "atribute":"gender" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+    {"question":"Does your character have hair color " , "atribute":"hair_color" , "value":"tba" , "efective":0, "isBool":false,"percentage":0},
+    {"question":"Is your character " , "atribute":"type" , "value":"tba" , "efective":0, "isBool":false, "percentage":0},
+    {"question":"Does your character talks?" , "atribute":"talks" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Is your character " , "atribute":"personality" , "value":"tba" , "efective":0, "isBool":false, "percentage":0},
+    {"question":"Does your character have a catch phrase?" , "atribute":"catch_phrase" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Does your character have his/her name in the show title?" , "atribute":"name_in_show_title" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Is your character from an educational show?" , "atribute":"educational" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Is your character the primary character?" , "atribute":"primary_character" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Does your character wears clothes?" , "atribute":"clothes" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Is your character a sidekick?" , "atribute":"sidekick" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+]
+
+var question_other_animated = [
+    {"question":"Is your character " , "atribute":"personality" , "value":"tba" , "efective":0, "isBool":false, "percentage":0},
+    {"question":"Is your character human like?" , "atribute":"human_like" , "value":"yes" , "efective":0,"isBool":true,"percentage":0},
+    {"question":"Is your character shaped like a " , "atribute":"shape" , "value":"tba" , "efective":0, "isBool":false, "percentage":0},
+]
+
+var stadistics = {"played":0, "human_real":0, "human_animated":0, "animal_real":0, "animal_animated":0, "other_animated":0,"guessed":0};
 
 
 // Question method
@@ -229,11 +234,50 @@ function question_maker(questions, copyCollection){
 	return possibleCharacter;
 }
 
-sendQuestion("Is your character real?");
 console.log("Welcome");
 //question_maker(questions_Human_real, copyCollection);
 
+  function gameOn(){
+    var posiblecharacter;
+    sendQuestion("Is your character real?");
+    if(answer === "yes"){
+        //codigo que lo hacer esperar otra respuesta
+        sendQuestion("Is your character a human?");
+        if(answer === "yes"){
+            //codigo que crea la copia de humano real
+            //posiblecharacter = question_maker(question_human_real, dbcopy)
+            console.log("Your group is human real");
+        }else{
+            //codigo que crea la copia de animal real
+            //posiblecharacter = question_maker(question_animal_real, dbcopy)
+            console.log("Your group is animal real");
+        }
+    }else{
+        //codigo que lo hacer esperar otra respuesta
+        sendQuestion("Is your character a human?");
+        if(answer === "yes"){
+            //codigo que crea copia de humano animado
 
- 
+            //posiblecharacter = question_maker(question_human_animated, dbcopy)
+            console.log("Your group is human animated");
+        }else{
+            //codigo que lo hacer esperar otra respuesta
+            sendQuestion("Is your character an Animal?");
+            if(answer === "yes"){
+                //codigo que crea la copia aniaml animado
+
+                //posiblecharacter = question_maker(question_animal_animated, dbcopy)
+                console.log("Your group is Animal animated");
+            }else{
+                //codigo que crea la copia otros
+
+                //posiblecharacter = question_maker(question_other_animated, dbcopy)
+                console.log("Your group is other animated");
+            }
+        }
+    }
+
+}
+
 
     
